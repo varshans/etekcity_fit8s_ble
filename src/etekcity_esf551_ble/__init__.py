876c9b2837430615a928df188bb25d05cc53f107
Ron <ron.benyosef@outlook.com>
2024-10-19 20:03:48 +0100

    Update __init__.py

--- src/etekcity_esf551_ble/__init__.py
+++ src/etekcity_esf551_ble/__init__.py
@@ -1,10 +1,11 @@
 from ._version import __version__
-from .body_metrics import BodyMetrics, Sex
+from .body_metrics import BodyMetrics, EtekcitySmartFitnessScaleWithBodyMetrics, Sex
 from .const import IMPEDANCE_KEY, WEIGHT_KEY
 from .parser import EtekcitySmartFitnessScale, ScaleData, WeightUnit
 
 __all__ = [
     "EtekcitySmartFitnessScale",
+    "EtekcitySmartFitnessScaleWithBodyMetrics",
     "WeightUnit",
     "ScaleData",
     "IMPEDANCE_KEY",
